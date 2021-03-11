66a8a1595df74d576b41b1570b7a23eb8a65dc4b
sunspcts <crynglghtng@gmail.com>
2021-03-11 14:00:45 +0000

    switch command handler from if statements to a much nicer switch/case block

--- main.js
+++ main.js
@@ -8,10 +8,10 @@ const Enmap = require('enmap');
 //db to store running games
 client.currentGames = new Enmap ({
 	name: "currentgames",
-  fetchAll: false,
-  autoFetch: true,
-  cloneLevel: 'deep',
-  autoEnsure: {
+	fetchAll: false,
+	autoFetch: true,
+	cloneLevel: 'deep',
+	autoEnsure: {
 		inSetup: false,
 		inGame: false,
 		day: 0,
@@ -21,7 +21,8 @@ client.currentGames = new Enmap ({
 		doneSpecialDay: false,
 		initialTributes: [],
 		currentTributes: [],
-  }
+		dead: [],
+	}
 });
 
 const config = require("./config.json") //contains prefix and token
@@ -30,51 +31,48 @@ prefix = config.prefix
 
 
 client.on('ready', () => {
-  console.log('bot online!');
-  client.user.setActivity('The Hunger Games', { type: 'PLAYING' }); 
+	console.log('bot online!');
+	client.user.setActivity('The Hunger Games', { type: 'PLAYING' }); 
 });
 
 client.on('message', message => {
-  if (message.author.bot) return;
+	if (message.author.bot) return;
 
-  if (!message.content.startsWith(prefix)) return;
-   //take away prefix
-  const args = message.content.slice(prefix.length).split(/ +/);
-  
+	if (!message.content.startsWith(prefix)) return;
+	 //take away prefix
+	const args = message.content.slice(prefix.length).split(/ +/);
+	
 	// make the command lowercase
 	const cmdName = args.shift().toLowerCase();
-
-  try {
-		if (cmdName === "setup") {
-			setup.startsetup(message, client)
+	
+	try {
+		switch(cmdName) {
+			case "setup":
+				setup.startsetup(message, client); break;
+			case "reset":
+				setup.reset(message, client); break;
+			case "uploadtributes":
+			case "addtribute":
+				if(client.currentGames.get(message.guild.id, "chosen")) message.channel.send("Setup already complete!");
+				if(!client.currentGames.get(message.guild.id, "inSetup")) message.channel.send(`Run ${config.prefix}setup first!`); 
+				break;
+			case "start":
+				if(!client.currentGames.get(message.guild.id, "inGame") && client.currentGames.get(message.guild.id, "chosen")) {
+					game.start(message, client) //HERE WE GO
+				}
+				break;
+			case "next":
+				if(client.currentGames.get(message.guild.id, "inGame")) {
+					game.next(message, client) //HERE WE GO
+				}
+				break;
 		}
-		if (cmdName === "reset") {
-			setup.reset(message, client)
-		}
-		//this is bad code
-		if (cmdName === "addtribute" || cmdName === "uploadtributes") {
-			if(client.currentGames.get(message.guild.id, "chosen")) {
-				message.channel.send("Setup already complete!");
-			}
-		}
-		console.log(client.currentGames.get(message.guild.id, "chosen"))
-		if (!client.currentGames.get(message.guild.id, "chosen")) {
-			if(client.currentGames.get(message.guild.id, "inSetup")) setup.chooseTributes(message, client);
-			else if (cmdName === "addtribute") message.channel.send(`Run ${config.prefix}setup first!`)
-		}
-		else {
-			console.log("yote")
-			console.log(cmdName)
-			if (cmdName === "start" && !client.currentGames.get(message.guild.id, "inGame") && client.currentGames.get(message.guild.id, "chosen")) { //HERE WE GO
-				console.log("yeetus")
-				game.start(message, client)
-			}
-		}
-  } catch (error) {
-  	console.error(error);
-	  message.channel.send("bot did an oopsie")
-  	//message.reply('something went wrong, but idk what. message my creator, sunspcts#0505 with a screenshot of what you were doing so they can try to fix it');
-  }
+		if(client.currentGames.get(message.guild.id, "inSetup")) setup.chooseTributes(message, client);
+	} catch (error) {
+		console.error(error);
+		message.channel.send("err")
+		//message.reply('something went wrong, but idk what. message my creator, sunspcts#0505 with a screenshot of what you were doing so they can try to fix it');
+	}
 });
 
 client.login(config.token)
